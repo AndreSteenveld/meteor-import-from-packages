5b491fb45b401a39c27dbea60cc2586fee463120
Andre Steenveld <andre.steenveld@gmail.com>
2016-06-23 13:25:28 +0200

    walk up the path

--- packages/imports-from-imports/imports-from-imports.js
+++ packages/imports-from-imports/imports-from-imports.js
@@ -3,4 +3,7 @@
 //import { exported_to_packages } from "/imports/exported-to-packages";
 //require( "/imports/exported-to-packages" );
 
+// This actually does find the file but throws a SyntaxError (code is not transpiled on the fly?)
+//Npm.require( "../../../../../imports/exported-to-packages" );
+
 imports_from_imports = 'imports-from-imports';
