8103c8d2e6cf7e17e69617b9de9d810ec6bb697a
Andre Steenveld <andre.steenveld@gmail.com>
2016-06-27 09:41:45 +0200

    Comment out the class with the properties

--- packages/package-with-class-properties/package-with-class-properties.js
+++ packages/package-with-class-properties/package-with-class-properties.js
@@ -2,9 +2,9 @@
 
 // Variables exported by this module can be imported by other packages and
 // applications. See package-with-class-properties-tests.js for an example of importing.
-export class IntializeMe {
-	property = "amazing";
-	constructor( ){
-		console.log( `All of this is [ ${ this.property } ]!` );
-	}
-}
+// export class IntializeMe {
+// 	property = "amazing";
+// 	constructor( ){
+// 		console.log( `All of this is [ ${ this.property } ]!` );
+// 	}
+// }
